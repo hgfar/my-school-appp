24e7a3ab52cc2d3566f8785b55673f1d5090e100
hgfar <88934720+hgfar@users.noreply.github.com>
2025-11-20 18:33:17 +0300

    refactor: Update Vite and dependencies in import map
    Updates the version of Vite and related dependencies in the import map for index.html. This also increments the service worker cache name to ensure new assets are fetched.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // sw.js (Stale-while-revalidate)
 
-const CACHE_NAME = 'school-schedule-pwa-cache-v15';
+const CACHE_NAME = 'school-schedule-pwa-cache-v21';
 const urlsToCache = [
   '/',
   '/index.html',
