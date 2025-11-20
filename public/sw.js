4984af22b15048477c2c7c01d2d1cec327cde568
hgfar <88934720+hgfar@users.noreply.github.com>
2025-11-20 19:15:22 +0300

    feat: Update PWA cache version
    Increments the PWA cache version to ensure updated assets are fetched by the service worker.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // sw.js (Stale-while-revalidate)
 
-const CACHE_NAME = 'school-schedule-pwa-cache-v25';
+const CACHE_NAME = 'school-schedule-pwa-cache-v26';
 const urlsToCache = [
   '/',
   '/index.html',
